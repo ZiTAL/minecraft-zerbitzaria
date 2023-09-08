dab3384a64d1335df2cd556941bafb342314a82c
bilbao_iban <bilbao_iban@eitb.eus>
2023-09-08 09:00:16 +0200

    bot telegram

--- scripts/bot/bot.py
+++ scripts/bot/bot.py
@@ -89,24 +89,7 @@ def masto(user, msg):
     mastodon.status_post(msg, visibility='public')
 
 def tele(user, msg):
-    """
-    *bold \*text*
-    _italic \*text_
-    __underline__
-    ~strikethrough~
-    ||spoiler||
-    *bold _italic bold ~italic bold strikethrough ||italic bold strikethrough spoiler||~ __underline italic bold___ bold*
-    [inline URL](http://www.example.com/)
-    [inline mention of a user](tg://user?id=123456789)
-    ![??](tg://emoji?id=5368324170671202286)
-    `inline fixed-width code`
-    ```
-    pre-formatted fixed-width code block
-    ```
-    ```python
-    pre-for
-    """    
-    msg     = "\*"+user+"*"+msg
+    msg     = "*"+user+"*"+msg
     token   = readFile(sys.path[0]+"/telegram.credentials").strip()
     channel = readFile(sys.path[0]+"/telegram.channel").strip()
     requests.get("https://api.telegram.org/bot"+token+"/sendMessage", params={
