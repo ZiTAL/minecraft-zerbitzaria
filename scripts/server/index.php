0f226b860ff7c307a76235516d348c5fcc3d33a4
bilbao_iban <bilbao_iban@eitb.eus>
2023-09-18 13:16:07 +0200

    server regex

--- scripts/server/index.php
+++ scripts/server/index.php
@@ -9,7 +9,7 @@ foreach($lines as $line)
 {
     if(preg_match("/UUID/i", $line))
         $line = preg_replace("/([a-z0-9]+\-[a-z0-9]+\-[a-z0-9]+\-[a-z0-9]+\-)[a-z0-9]+/i", '$1************', $line);
-    $line = preg_replace("/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\.:[0-9]+/", '*.*.*.*.:*', $line);
+    $line = preg_replace("/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+/", '*.*.*.*.:*', $line);
     $log .= "{$line}\n";
 }
 
